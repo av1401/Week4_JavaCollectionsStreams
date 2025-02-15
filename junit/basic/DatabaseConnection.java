02b72708b94f77f94afd7c67eacd23fc92abbaf7
av1401 <rsabhi1401@gmail.com>
2025-02-15 16:23:23 +0530

    [ABHISHEK VISHWAKARMA] Add . Basic Problem 5 : Database Connection

---
+++ junit/basic/DatabaseConnection.java
@@ -0,0 +1,19 @@
+package com.junit.basic;
+
+public class DatabaseConnection {
+    private boolean isConnected = false;
+
+    public void connect() {
+        isConnected = true;
+        System.out.println("Database connected.");
+    }
+
+    public void disconnect() {
+        isConnected = false;
+        System.out.println("Database disconnected.");
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+}
