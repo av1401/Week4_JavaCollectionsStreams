84a72bf8eea6501c46fce459569877a441c24a71
av1401 <rsabhi1401@gmail.com>
2025-02-15 16:49:31 +0530

    [ABHISHEK VISHWAKARMA] Add . Basic Problem 6

---
+++ junit/basic/IsEven.java
@@ -0,0 +1,10 @@
+package com.junit.basic;
+
+public class IsEven {
+    public static boolean isEven(int n){
+        if(n % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+}
