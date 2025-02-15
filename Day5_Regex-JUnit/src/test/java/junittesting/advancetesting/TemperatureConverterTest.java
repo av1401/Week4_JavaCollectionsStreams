4c3c31fce72b8de1bda17ef628422bef0e6ed2ee
av1401 <rsabhi1401@gmail.com>
2025-02-15 23:14:56 +0530

    [ABHISHEK VISHWAKARMA] Add .  Problem 3

---
+++ Day5_Regex-JUnit/src/test/java/junittesting/advancetesting/TemperatureConverterTest.java
@@ -0,0 +1,31 @@
+package junittesting.advancetesting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.junit.advance.TemperatureConverter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TemperatureConverterTest {
+
+    private TemperatureConverter converter;
+
+    @BeforeEach
+    public void setUp() {
+        converter = new TemperatureConverter();
+    }
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        assertEquals(32.0, converter.celsiusToFahrenheit(0), 0.001);
+        assertEquals(212.0, converter.celsiusToFahrenheit(100), 0.001);
+        assertEquals(98.6, converter.celsiusToFahrenheit(37), 0.001);
+    }
+
+    @Test
+    public void testFahrenheitToCelsius() {
+        assertEquals(0.0, converter.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100.0, converter.fahrenheitToCelsius(212), 0.001);
+        assertEquals(37.0, converter.fahrenheitToCelsius(98.6), 0.001);
+    }
+}
