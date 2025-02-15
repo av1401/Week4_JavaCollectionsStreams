29b35754f49553c1ed114aee3b4e2d2e31df5f4a
av1401 <rsabhi1401@gmail.com>
2025-02-15 22:53:05 +0530

    [ABHISHEK VISHWAKARMA] Add .  Problem 7

---
+++ Day5_Regex-JUnit/src/main/java/com/junit/basic/PerformanceTester.java
@@ -0,0 +1,16 @@
+package com.junit.basic;
+
+
+
+public class PerformanceTester {
+
+    // Sleep for 3 seconds and return a result
+    public String longRunningTask() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return "done";
+    }
+}
---
+++ Day5_Regex-JUnit/src/test/java/junittesting/basic/PerformanceTesterTest.java
@@ -0,0 +1,21 @@
+package junittesting.basic;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.junit.basic.PerformanceTester;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import java.util.concurrent.TimeUnit;
+
+public class PerformanceTesterTest {
+
+    PerformanceTester tester = new PerformanceTester();
+
+    @Test
+    @Timeout(value = 2, unit = TimeUnit.SECONDS)
+    public void testLongRunningTask() {
+        // This test should fail due to timeout
+        assertEquals("done", tester.longRunningTask());
+    }
+}
